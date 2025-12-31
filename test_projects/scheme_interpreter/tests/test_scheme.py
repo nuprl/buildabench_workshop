400c665447f897a527224d81465ec572530d5e73
Arjun Guha <a.guha@northeastern.edu>
2025-12-31 06:52:35 -0500

    Fixed up test project

--- test_projects/scheme_interpreter/tests/test_scheme.py
+++ test_projects/scheme_interpreter/tests/test_scheme.py
@@ -1,6 +1,6 @@
-import unittest
+import pytest
 
-from test_projects.scheme_interpreter.scheme import (
+from scheme_interpreter import (
     SchemeError,
     eval_expr,
     parse,
@@ -11,92 +11,101 @@ from test_projects.scheme_interpreter.scheme import (
 )
 
 
-class SchemeInterpreterTests(unittest.TestCase):
-    def test_parse_numbers_and_symbols(self):
-        self.assertEqual(parse("42"), 42)
-        self.assertEqual(parse("-7"), -7)
-        self.assertEqual(parse("3.5"), 3.5)
-        self.assertEqual(parse("foo"), "foo")
-
-    def test_parse_list(self):
-        self.assertEqual(parse("(+ 1 2)"), ["+", 1, 2])
-
-    def test_parse_many(self):
-        expressions = parse_many("(+ 1 2) (* 2 3)")
-        self.assertEqual(expressions, [["+", 1, 2], ["*", 2, 3]])
-
-    def test_to_string_roundtrip(self):
-        expr = parse("(if #t (+ 1 2) (- 5 3))")
-        self.assertEqual(to_string(expr), "(if #t (+ 1 2) (- 5 3))")
-
-    def test_arithmetic(self):
-        env = standard_env()
-        self.assertEqual(eval_expr(parse("(+ 1 2 3)"), env), 6)
-        self.assertEqual(eval_expr(parse("(- 10 3 2)"), env), 5)
-        self.assertEqual(eval_expr(parse("(- 5)"), env), -5)
-        self.assertEqual(eval_expr(parse("(* 2 3 4)"), env), 24)
-        self.assertAlmostEqual(eval_expr(parse("(/ 8 2)"), env), 4)
-        self.assertAlmostEqual(eval_expr(parse("(/ 2)"), env), 0.5)
-
-    def test_comparisons(self):
-        env = standard_env()
-        self.assertTrue(eval_expr(parse("(> 5 3 1)"), env))
-        self.assertFalse(eval_expr(parse("(> 5 5)"), env))
-        self.assertTrue(eval_expr(parse("(<= 2 2 3)"), env))
-        self.assertTrue(eval_expr(parse("(= 4 4 4)"), env))
-
-    def test_if_expression(self):
-        env = standard_env()
-        self.assertEqual(eval_expr(parse("(if #t 1 2)"), env), 1)
-        self.assertEqual(eval_expr(parse("(if #f 1 2)"), env), 2)
-
-    def test_lambda_application(self):
-        env = standard_env()
-        expression = parse("((lambda (x y) (+ x y)) 3 4)")
-        self.assertEqual(eval_expr(expression, env), 7)
-
-    def test_lambda_closure(self):
-        env = standard_env()
-        expression = parse("(((lambda (x) (lambda (y) (+ x y))) 5) 7)")
-        self.assertEqual(eval_expr(expression, env), 12)
-
-    def test_higher_order_function(self):
-        env = standard_env()
-        expression = parse("((lambda (f x) (f x)) (lambda (n) (* n n)) 6)")
-        self.assertEqual(eval_expr(expression, env), 36)
-
-    def test_variable_shadowing(self):
-        env = standard_env()
-        expression = parse("((lambda (x) ((lambda (x) (+ x 1)) 10)) 5)")
-        self.assertEqual(eval_expr(expression, env), 11)
-
-    def test_closure_outlives_scope(self):
-        env = standard_env()
-        expression = parse(
-            "((lambda (x) ((lambda (f) (f 5)) (lambda (y) (+ x y)))) 20)"
-        )
-        self.assertEqual(eval_expr(expression, env), 25)
-
-    def test_quote(self):
-        env = standard_env()
-        expression = parse("(quote (1 2 3))")
-        self.assertEqual(eval_expr(expression, env), [1, 2, 3])
-
-    def test_run_multiple(self):
-        program = "(+ 1 2) (* 3 4)"
-        self.assertEqual(run(program), 12)
-
-    def test_errors(self):
-        env = standard_env()
-        with self.assertRaises(SchemeError):
-            eval_expr(parse("()"), env)
-        with self.assertRaises(SchemeError):
-            eval_expr(parse("(if #t 1)"), env)
-        with self.assertRaises(SchemeError):
-            eval_expr(parse("(lambda x x)"), env)
-        with self.assertRaises(SchemeError):
-            eval_expr(parse("(unknown 1 2)"), env)
+def test_parse_numbers_and_symbols():
+    assert parse("42") == 42
+    assert parse("-7") == -7
+    assert parse("3.5") == 3.5
+    assert parse("foo") == "foo"
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_parse_list():
+    assert parse("(+ 1 2)") == ["+", 1, 2]
+
+
+def test_parse_many():
+    expressions = parse_many("(+ 1 2) (* 2 3)")
+    assert expressions == [["+", 1, 2], ["*", 2, 3]]
+
+
+def test_to_string_roundtrip():
+    expr = parse("(if #t (+ 1 2) (- 5 3))")
+    assert to_string(expr) == "(if #t (+ 1 2) (- 5 3))"
+
+
+def test_arithmetic():
+    env = standard_env()
+    assert eval_expr(parse("(+ 1 2 3)"), env) == 6
+    assert eval_expr(parse("(- 10 3 2)"), env) == 5
+    assert eval_expr(parse("(- 5)"), env) == -5
+    assert eval_expr(parse("(* 2 3 4)"), env) == 24
+    assert eval_expr(parse("(/ 8 2)"), env) == pytest.approx(4)
+    assert eval_expr(parse("(/ 2)"), env) == pytest.approx(0.5)
+
+
+def test_comparisons():
+    env = standard_env()
+    assert eval_expr(parse("(> 5 3 1)"), env) is True
+    assert eval_expr(parse("(> 5 5)"), env) is False
+    assert eval_expr(parse("(<= 2 2 3)"), env) is True
+    assert eval_expr(parse("(= 4 4 4)"), env) is True
+
+
+def test_if_expression():
+    env = standard_env()
+    assert eval_expr(parse("(if #t 1 2)"), env) == 1
+    assert eval_expr(parse("(if #f 1 2)"), env) == 2
+
+
+def test_lambda_application():
+    env = standard_env()
+    expression = parse("((lambda (x y) (+ x y)) 3 4)")
+    assert eval_expr(expression, env) == 7
+
+
+def test_lambda_closure():
+    env = standard_env()
+    expression = parse("(((lambda (x) (lambda (y) (+ x y))) 5) 7)")
+    assert eval_expr(expression, env) == 12
+
+
+def test_higher_order_function():
+    env = standard_env()
+    expression = parse("((lambda (f x) (f x)) (lambda (n) (* n n)) 6)")
+    assert eval_expr(expression, env) == 36
+
+
+def test_variable_shadowing():
+    env = standard_env()
+    expression = parse("((lambda (x) ((lambda (x) (+ x 1)) 10)) 5)")
+    assert eval_expr(expression, env) == 11
+
+
+def test_closure_outlives_scope():
+    env = standard_env()
+    expression = parse(
+        "((lambda (x) ((lambda (f) (f 5)) (lambda (y) (+ x y)))) 20)"
+    )
+    assert eval_expr(expression, env) == 25
+
+
+def test_quote():
+    env = standard_env()
+    expression = parse("(quote (1 2 3))")
+    assert eval_expr(expression, env) == [1, 2, 3]
+
+
+def test_run_multiple():
+    program = "(+ 1 2) (* 3 4)"
+    assert run(program) == 12
+
+
+def test_errors():
+    env = standard_env()
+    with pytest.raises(SchemeError):
+        eval_expr(parse("()"), env)
+    with pytest.raises(SchemeError):
+        eval_expr(parse("(if #t 1)"), env)
+    with pytest.raises(SchemeError):
+        eval_expr(parse("(lambda x x)"), env)
+    with pytest.raises(SchemeError):
+        eval_expr(parse("(unknown 1 2)"), env)
